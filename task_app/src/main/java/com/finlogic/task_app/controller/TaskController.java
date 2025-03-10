4e4241f01c9a3e7a4ec823c6deb9173a7a159e0c
thisisdishang <dishangkumarrana@gmail.com>
2025-03-10 22:41:11 +0530

    First Spring Boot App

---
+++ task_app/src/main/java/com/finlogic/task_app/controller/TaskController.java
@@ -0,0 +1,18 @@
+package com.finlogic.task_app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// @RestController = @Controller + @ResponseBody
+@RequestMapping("/task")
+public class TaskController {
+
+    @GetMapping
+    public String getMessage() {
+        return "Hello Spring Boot";
+    }
+}
---
+++ task_app/src/test/java/com/finlogic/task_app/TaskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finlogic.task_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
