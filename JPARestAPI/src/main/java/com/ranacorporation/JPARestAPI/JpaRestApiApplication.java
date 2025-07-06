54ddd6ce3d41a740bc69b6b054d957d9bc7516e2
thisisdishang <dishangkumarrana@gmail.com>
2025-07-06 18:45:45 +0530

    Initialize first JPA based spring boot project

---
+++ JPARestAPI/src/main/java/com/ranacorporation/JPARestAPI/JpaRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ranacorporation.JPARestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaRestApiApplication.class, args);
+	}
+
+}
---
+++ JPARestAPI/src/test/java/com/ranacorporation/JPARestAPI/JpaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranacorporation.JPARestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
