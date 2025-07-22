e4501742b651042a1e0f45bd8c5d13b17dadd3c8
thisisdishang <dishangkumarrana@gmail.com>
2025-07-22 18:45:45 +0530

    Try to secure API with static basic authentitcation

--- JPARestAPI/src/main/java/com/ranacorporation/JPARestAPI/config/SecurityConfig.java
+++ JPARestAPI/src/main/java/com/ranacorporation/JPARestAPI/config/SecurityConfig.java
@@ -1,4 +1,43 @@
 package com.ranacorporation.JPARestAPI.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/employees").authenticated()
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .httpBasic(); // Enables Basic Auth
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails user = User.withUsername("Kubera")
+                .password(encoder.encode("jaibhole"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
