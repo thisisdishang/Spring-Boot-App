a387c0210ef8022ddbb03d2628f4a5feb0852ddb
thisisdishang <dishangkumarrana@gmail.com>
2025-07-24 18:45:45 +0530

    Implement DAO for fetch Auth User data

--- JPARestAPI/src/main/java/com/ranacorporation/JPARestAPI/business/repository/AuthUserRepository.java
+++ JPARestAPI/src/main/java/com/ranacorporation/JPARestAPI/business/repository/AuthUserRepository.java
@@ -1,4 +1,10 @@
 package com.ranacorporation.JPARestAPI.business.repository;
 
-public class AuthUserRepository {
+import com.ranacorporation.JPARestAPI.entity.AuthUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AuthUserRepository extends JpaRepository<AuthUser, Integer> {
+    Optional<AuthUser> findByUsername(String username);
 }
