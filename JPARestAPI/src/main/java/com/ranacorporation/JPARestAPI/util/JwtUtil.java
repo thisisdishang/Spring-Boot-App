612e23643166382672f337a440d285a7d1300e4d
thisisdishang <dishangkumarrana@gmail.com>
2025-07-27 18:45:45 +0530

    Made JwtUtil for generate and validate token

--- JPARestAPI/src/main/java/com/ranacorporation/JPARestAPI/util/JwtUtil.java
+++ JPARestAPI/src/main/java/com/ranacorporation/JPARestAPI/util/JwtUtil.java
@@ -1,4 +1,38 @@
 package com.ranacorporation.JPARestAPI.util;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
 public class JwtUtil {
+    private static final String SECRET = "mysecretkey1234567890123456789012";
+    // Common signing key
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token, org.springframework.security.core.userdetails.UserDetails userDetails) {
+        String username = extractUsername(token);
+        return username.equals(userDetails.getUsername());
+    }
 }
