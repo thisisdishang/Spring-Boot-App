f40434b87c774c74f1c44d893eee1011a16ec6fc
thisisdishang <dishangkumarrana@gmail.com>
2025-07-30 18:45:45 +0530

    Create AuthController for generate the token based on valid username and password

--- JPARestAPI/src/main/java/com/ranacorporation/JPARestAPI/web/controller/AuthController.java
+++ JPARestAPI/src/main/java/com/ranacorporation/JPARestAPI/web/controller/AuthController.java
@@ -1,4 +1,38 @@
 package com.ranacorporation.JPARestAPI.web.controller;
 
+import com.ranacorporation.JPARestAPI.entity.AuthUser;
+import com.ranacorporation.JPARestAPI.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authManager;
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthUser request) {
+        authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+        );
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+        final String token = jwtUtil.generateToken(userDetails.getUsername());
+
+        return ResponseEntity.ok(
+                java.util.Collections.singletonMap("token", token)
+        );
+    }
+
 }
+
